8763c365acf3c77e980956ef358262e8af4d2233
Sienna <sienna.interwebs@proton.me>
2026-09-25 19:05:55 -0400

    encapsulate queue state and add behavioral tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
-
 #include<iostream>
 class Queue {
     private:
-
-    public:
 
         int collection[5];
         int position;
         int put_index;
         int count;
 
+    public:
         Queue() {
             this->position = 0;
             this->put_index = 0;
@@ -38,34 +36,13 @@ class Queue {
 
 };
 
-void print_all(Queue& q) {
-    for (int i{}; i < 5; i++) {
-        std::cout << q.collection[i] << "\n";
-    }
-}
-
-void test_fifo(Queue& q) {
-    int value = 100;
-    for (int i = 0; i < 5; i++) {
-        q.put(value);
-        value *= 2;
-    }
-    print_all(q);
-
-    for (int i{}; i < 5; i++) {
-        q.take(value);
-        value /= 2;
-    }
-    print_all(q);
-}
-
 void test_empty_queue(Queue& q) {
     int value;
     bool result = q.take(value);
     if (result) {
-        std::cout << "test failed: empty queue has been taken";
+        std::cout << "test failed: empty queue has been taken" << "\n";
     } else {
-        std::cout << "test passed: empty queue has not been taken";
+        std::cout << "test passed: empty queue has not been taken" << "\n";
     }
 }
 
@@ -73,9 +50,9 @@ void test_full_queue(Queue& q) {
     int value = 100;
     bool result = q.put(value);
     if (result) {
-        std::cout << "test failed: full queue has been filled";
+        std::cout << "test failed: full queue has been filled" << "\n";
     } else {
-        std::cout << "test passed: full queue has not been filled";
+        std::cout << "test passed: full queue has not been filled" << "\n";
     }
 }
 
@@ -88,56 +65,88 @@ void arbitrary_fill(Queue& q) {
 
 void test_ring(Queue& q) {
     int value;
-    q.put(100);
-    if (q.put_index == 1) {
-        std::cout << "test passed: empty ring has been filled with 1 element" << "\n";
-    } else {
-        std::cout << "test failed: empty ring has not been filled with 1 element" << "\n";
+    bool result = q.put(100);
+
+    if (result) {
+        std::cout << "oh hey, the insertion works. passed :)" << "\n";
     }
-    q.put(200);
-    if (q.put_index == 2) {
-        std::cout << "test passed: empty ring has been filled with 2 elements" << "\n";
-    } else {
-        std::cout << "test failed: empty ring has not been filled with 2 elements" << "\n";
-    }
-    q.put(300);
-    if (q.put_index == 3) {
-        std::cout << "test passed: empty ring has been filled with 3 elements" << "\n";
-    } else {
-        std::cout << "test failed: empty ring has not been filled with 3 elements" << "\n";
-    }
-    q.take(value);
-    if (q.position == 1 && value == 100) {
-        std::cout << "test passed: element taken from empty ring" << "\n";
-    } else {
-        std::cout << "test failed: element taken from empty ring" << "\n";
+    else {
+        std::cout << "noo insertion doesn't work, failed :((" << "\n";
     }
 
+    q.put(400);
     q.put(500);
-    if (q.put_index == 4 && q.count == 3) {
-        std::cout << "test passed: ring has been filled with 3 elements" << "\n";
-    } else {
-        std::cout << "test failed: either count or put_index is incorrect" << "\n";
+    q.put(600);
+
+    result = q.take(value);
+
+    if (result) {
+        std::cout << "yay! take() is working. passed :)" << "\n";
     }
+    else {
+        std::cout << "noo something is wrong with take(), failed :(" << "\n";
+    }
+
+    q.put(1200);
+    q.put(8000);
+
+    result = q.put(56);
+
+    if (result) {
+        std::cout << "noo something is wrong with put(), failed :(" << "\n";
+    }
+    else {
+        std::cout << "yay! put() is working. passed :)" << "\n";
+    }
+    std::cout << "okay, let's test FIFO popping out values now." << "\n";
+
     q.take(value);
-    if (q.count == 2 && q.position == 2 && value == 200) {
-        std::cout << "test passed: element taken from ring" << "\n";
-    } else {
-        std::cout << "test failed: element taken from ring" << "\n";
+    if (value == 400) {
+        std::cout << "expected " << value << ", got " << value << ". sweet :)" << "\n";
     }
-    q.put(700);
-    if (q.put_index == 0 && q.count == 3) {
-        std::cout << "test passed: ring has been filled with 3 elements" << "\n";
-    } else {
-        std::cout << "test failed: either count or put_index is incorrect" << "\n";
+    else {
+        std::cout << "oh FUCK something's wrong with FIFO god damn it";
     }
+
+    q.take(value);
+    if (value == 500) {
+        std::cout << "expected " << value << ", got " << value << ". sweet :)" << "\n";
+    }
+    else {
+        std::cout << "oh FUCK something's wrong with FIFO god damn it";
+    }
+
+    q.take(value);
+    if (value == 600) {
+        std::cout << "expected " << value << ", got " << value << ". sweet :)" << "\n";
+    }
+    else {
+        std::cout << "oh FUCK something's wrong with FIFO god damn it";
+    }
+
+    q.take(value);
+    if (value == 1200) {
+        std::cout << "expected " << value << ", got " << value << ". sweet :)" << "\n";
+    }
+    else {
+        std::cout << "oh FUCK something's wrong with FIFO god damn it";
+    }
+
+    q.take(value);
+    if (value == 8000) {
+        std::cout << "expected " << value << ", got " << value << ". sweet :)" << "\n";
+    }
+    else {
+        std::cout << "oh FUCK something's wrong with FIFO god damn it" << "\n";
+    }
+
 }
+
 
 int main() {
     Queue q;
     Queue p;
     Queue r;
-    test_fifo(q);
     test_empty_queue(p);
     test_ring(p);
     arbitrary_fill(r);
